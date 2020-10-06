828d00777fa63d436cf876b002ebbfd4e90e1ca6
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-10-06 05:12:50 -0500

    Updates

---
+++ precache-manifest.d60f391352f71b4dcca3f84dbd8a9778.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b3fb9ad97b9630e7bfd402d4c3ef94",
+    "url": "/eduardobonilla/index.html"
+  },
+  {
+    "revision": "328d3fd1f2ce4792919e",
+    "url": "/eduardobonilla/static/css/main.07ad5e37.chunk.css"
+  },
+  {
+    "revision": "b8aa4bee18027e4bbefd",
+    "url": "/eduardobonilla/static/js/2.9fbe1b5d.chunk.js"
+  },
+  {
+    "revision": "f309080c7084103ee1d148adf8ef7d82",
+    "url": "/eduardobonilla/static/js/2.9fbe1b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328d3fd1f2ce4792919e",
+    "url": "/eduardobonilla/static/js/main.42f81067.chunk.js"
+  },
+  {
+    "revision": "01cdafc8a45aaf401d21",
+    "url": "/eduardobonilla/static/js/runtime-main.6c9e2a18.js"
+  },
+  {
+    "revision": "2f45fde7abeba87c8eddbb1e03e429c3",
+    "url": "/eduardobonilla/static/media/EvolveSansMedium.2f45fde7.otf"
+  },
+  {
+    "revision": "61a0ace347c6da870948649ab56c35f8",
+    "url": "/eduardobonilla/static/media/EvolveSansMediumEVO.61a0ace3.otf"
+  }
+]);
